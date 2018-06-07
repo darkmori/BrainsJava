3650ae09c8aa0c5b140b3b228962962516aa5c44
r1-21 <r1-21@JeongKS>
2018-06-07 15:22:23 +0900

    06-07

--- 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
+++ 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
@@ -23,11 +23,23 @@ public class MybatisTest {
 				// Blog blog =
 				// session.selectOne("org.mybatis.example.BlogMapper.selectBlog",101);
 				// Member member=new Member();
-				Member member = session.selectOne("org.mybatis.example.SelectMapper.selectMember", 101);
+				Member member = (Member) session.selectOne("org.mybatis.example.SelectMapper.selectMember", 101);
+				System.out.println("회원 아이디: " + member.getId());
+				///////////// 추가///////////////
+				
+				// Member newMember = new Member(102, "testID", "1234");
+				
+				Member newMember = new Member();
+				newMember.setMno(102);
+				newMember.setId("testID");
+				newMember.setPassword("1234");
 
-				System.out.println("회원 아이디: " + member.getPassword());
+				///////////// 삭제///////////////
+				int deleteCount = session.delete("org.mybatis.example.SelectMapper.deleteMember", 101);
+				System.out.println("삭제 건수:" + deleteCount);
 
 			} finally {
+				session.commit();
 				session.close();
 			}
 		} catch (IOException e) {
